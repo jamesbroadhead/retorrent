b37ab63f95db5e624ebaeddc01239d9a7ba3ddfb
James Broadhead <jamesbroadhead@gmail.com>
2012-06-05 19:02:58 +0100

    patch to home-deploy

--- scripts/home-deploy.sh
+++ scripts/home-deploy.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-for i in ${HOME}/checkouts/twitter-home/dot_* ; do
-	dot_name="$(basename ${i})"
-    dot_path="$(dirname ${i})/${dot_name}"
-	newname="$(basename ${i/dot_/.})"
+DEPLOYDIR="$1"
 
-	if [ -e ${HOME}/$newname ] && ! [ "$(readlink ${HOME}/${newname})" = "${dot_path}" ] ; then
-		echo "$newname already existed --> ${newname}.bak"
+for i in $1/dot_* ; do
+    dot_name="$(basename ${i})"
+    dot_path="$(dirname ${i})/${dot_name}"
+    newname="$(basename ${i/dot_/.})"
+    newpath="${HOME}/${newname}"
+
+    if [ -e "${newpath}" ] || [ -L "${newpath}" ] ; then 
+	
+	# broken symlink in place
+	if ! [ -f "${HOME}/$newname" ] && [ -L "${HOME}/$newname" ] ; then 
+		rm "$newpath"
+	# not a symlink pointing to the correct location
+	elif ! [ "$(readlink ${HOME}/${newname})" = "${dot_path}" ] ; then
+		echo "$newname already existed! $newname --> ${newname}.bak"
 		mv -i "${HOME}/${newname}" "${HOME}/${newname}.bak"
+	
+	# for simplicity, remove symlinks pointing to the correct location
+   	else
+		rm $newpath
+	fi
     fi
-    if [ "$(readlink ${HOME}/${newname})" = "${dot_path}" ] ; then
-        echo "Skipping $newname"
-    else
-        ln -s "$dot_path" ${HOME}/${newname}
-    fi
+
+    ln -s "$dot_path" ${HOME}/${newname}
+done
+
+echo "==============="
+echo " need manual attention: "
+for i in $1/inside* ; do
+	echo "$i"
 done
