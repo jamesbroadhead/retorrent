ffbf566ea79e76802984a7abd8fecf073c281544
James Broadhead <jamesbroadhead@gmail.com>
2012-09-09 16:22:45 +0100

    retorrent: remove .py from bin/*

--- retorrent/setup.py
+++ retorrent/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup 
+from distutils.core import setup
 
 setup(name='retorrent',
 	version='0.1',
@@ -10,12 +10,12 @@ setup(name='retorrent',
 	url='http://code.google.com/p/jamesbroadhead',
 	packages = ['retorrentlib', 'os_utils', 'logdecorators'],
 	package_dir = {'': 'lib'},
-	data_files=[ ('share/retorrent', 
-					['conf/divider_symbols.conf', 
-					 'conf/fileext_details.conf', 
+	data_files=[ ('share/retorrent',
+					['conf/divider_symbols.conf',
+					 'conf/fileext_details.conf',
 					 'conf/retorrent.conf_skel'])],
-	scripts=['bin/is_seeded.py',
-			 'bin/remreffer.py',
-			 'bin/retorrent.py',
-			 'bin/symlinker.py']
+	scripts=['bin/is_seeded',
+			 'bin/remreffer',
+			 'bin/retorrent',
+			 'bin/symlinker']
 	)
