cbe4c31169eba80425c73b788e1a1dcb5b15d919
James Broadhead <jamesbroadhead@gmail.com>
2013-01-01 23:52:24 +0000

    ADSfdasf

--- scripts/git-clearbranches.sh
+++ scripts/git-clearbranches.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-git checkout master
+if [ $1 = '' ] ; then
+    basebranch="master"
+else
+    basebranch="$1"
+fi
+git checkout $basebranch
 git fetch
 git remote prune origin
 # remove all local branches merged into master
-git branch --merged master | grep -v 'master$' | xargs git branch -d
+git branch --merged $basebranch | grep -v 'master$' | xargs git branch -d
