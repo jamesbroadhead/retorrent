0bff96998431eb19dca99fb5cf4be211e465c359
James Broadhead <jamesbroadhead@gmail.com>
2013-04-21 13:45:59 +0100

    Add 'cd' context handler from SO.

---
+++ retorrent/lib/os_utils/context_handlers.py
@@ -0,0 +1,17 @@
+
+import os
+
+class cd:
+    """
+    http://stackoverflow.com/a/13197763
+    """
+
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
