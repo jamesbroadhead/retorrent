3a89e78c1e66444c4e8a6d2405f565605dfdabc6
James Broadhead <jamesbroadhead@gmail.com>
2014-02-02 01:36:02 +0000

    Improve is_seeded output

--- retorrent/lib/retorrentlib/seededlib.py
+++ retorrent/lib/retorrentlib/seededlib.py
@@ -1,41 +1,45 @@
 import os
 
+from os.path import abspath, expanduser, isdir, realpath
+from os.path import join as pjoin
+
 def is_seeded(args):
-    seeddir=os.path.expanduser('~/seed')
-    seed_map = [ (root,dirs,files) for root, dirs, files in os.walk(seeddir, followlinks=True) ] 
-    
-    seed_filepaths = []
-    
-    for root,dir,files in seed_map:
-        # build a flat list of the defeferenced symlinks. 
-        seed_filepaths.append(os.path.realpath(root))
-        seed_filepaths.extend([os.path.realpath(os.path.join(root,file)) for file in files])    
-    
-    seeded = []
+    seeddir = expanduser('~/seed')
+    seed_map = [ (root,dirs,files) for root, dirs, files in os.walk(seeddir, followlinks=True) ]
+
+    seed_filepaths = {}
+
+    for root, dir_, files in seed_map:
+        # build a flat list of the defeferenced symlinks.
+        seed_filepaths[realpath(root)] = abspath(root)
+        seed_filepaths.update({ realpath(pjoin(root, file_)) : pjoin(root, file_) for file_ in files })
+
+    seeded = {}
     unseeded = []
     for arg in args:
-        if is_seeded_singleitem(arg,seed_filepaths):
-            seeded += [arg]
+        res = is_seeded_singleitem(arg, seed_filepaths)
+        if res:
+            seeded[arg] = res
         else:
-            unseeded += [arg]
-    
-    return seeded,unseeded
+            unseeded.append(arg)
+
+    return seeded, unseeded
 
 # Passed a file -> Somewhere in seed is a symlink to that file
 # Passed a dir ->     1./ Somewhere in seed is a symlink to that dir
 #                    2./ Somewhere in seed is a symlink to the dir's contents
 def is_seeded_singleitem(path, seed_filepaths):
-    the_path = os.path.realpath(path)
+    the_path = realpath(path)
 
-    for each_file in seed_filepaths:
-        if the_path in each_file: 
-            return True 
+    for derefd_symlink, symlink in seed_filepaths.items():
+        if the_path == derefd_symlink:
+            return symlink
 
     # let's now look at dir contents
-    if os.path.isdir(the_path):
+    if isdir(the_path):
         for each_dir in os.listdir(the_path):
-            if is_seeded_singleitem(\
-                    os.path.join(the_path, each_dir),seed_filepaths):
-                return True
-    return False 
+            res = is_seeded_singleitem(pjoin(the_path, each_dir),seed_filepaths)
+            if res:
+                return res
+    return None
 
