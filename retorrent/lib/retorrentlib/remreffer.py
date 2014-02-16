aba3f30ce5d27f1af28302d918874fbe9353b429
James Broadhead <jamesbroadhead@gmail.com>
2014-02-16 23:21:18 +0000

    Move remreffer logic into a lib

---
+++ retorrent/lib/retorrentlib/remreffer.py
@@ -0,0 +1,47 @@
+import os
+from subprocess import Popen
+
+def remref(args):
+    paths = trim_trailing_slashes(args)
+
+    print "Would delete:"
+    pathlist = []
+    for item in paths:
+        pathlist.extend(add_item(item))
+
+    for item in pathlist:
+        print item
+
+    command = ['rm','-Irv']
+    command.extend(pathlist)
+
+    p = Popen(command)
+    sts = os.waitpid(p.pid, 0)[1]
+    return sts
+
+def add_item(item):
+    pathlist = []
+
+
+    # add the item. if it's a symlink, add the symlink
+    if os.path.exists(item):
+        pathlist.append(os.path.abspath(item))
+
+    if os.path.islink(item):
+        # add the real item
+        if os.path.exists(os.path.realpath(item)):
+            pathlist.append(os.path.abspath(os.path.realpath(item)))
+
+    # isdir(symlink) == True
+    if os.path.isdir(item):
+        for subitem in os.listdir(item):
+            pathlist.extend(add_item(os.path.join(item,subitem)))
+
+    pathlist.reverse()
+
+    return pathlist
+
+def trim_trailing_slashes(args):
+    return [ arg[0:-1] if arg[-1] == '/' else arg for arg in args  ]
+
+
